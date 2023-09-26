32e4c34f722e2cd3e2979ed574c7b036ffa6bfa4
Ritik kumar <ritikkumarkashyap9@gmail.com>
2023-09-26 15:51:10 +0530

    config changes

---
+++ admin-panel/src/components/constent.js
@@ -0,0 +1,2 @@
+//export const BASEURL = 'http://13.234.239.76:5000/';
+export const BASEURL = 'http://localhost:5000/';
--- bakend/db/User.js
+++ bakend/db/User.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name : String,
-  email : String,
-  password:String,
-age : Number
-
+  name: String,
+  email: String,
+  contact: Number,
+  expiary: new Date().toDateString(),
+  selectedOption1: String,
+  selectedOption2: String,
+  selectedOption3: String,
+  selectedOption4: String,
+  selectedOption5: String,  // Add this state for the select option
+  message: String,
 
 
 });
