40ee81597ffda3a0079c0c9eb6b5d2bbcc60879d
Ritik kumar <ritikkumarkashyap9@gmail.com>
2023-10-17 15:20:16 +0530

    Cron Token changed with actual token

--- bakend/CronSchadule.js
+++ bakend/CronSchadule.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const userSchema = require("./db/employee");
 
 
-var iscronStart = 0;  //only testing purpose
+var iscronStart = 1;  //only testing purpose
 //Call the function to get expired employees
 cron.schedule('* * * * *', () => {
 
@@ -88,7 +88,7 @@ async function SendWhatsApp(mobileNo) {
         // Define the data you want to send to the third-party API
         const postData = {
             "messaging_product": "whatsapp",
-            "to": "919711812800",
+            "to": "919049174551",
             "type": "template",
             "template": {
                 "name": "hello_world",
@@ -100,7 +100,7 @@ async function SendWhatsApp(mobileNo) {
         };
 
         // Replace 'YOUR_BEARER_TOKEN' with your actual token
-        const bearerToken = 'EAAEf48MREagBOZB6Hr7yJtGv8Sf4klPpPXmZAAT1uubqn87YgGHLO3kD6ZBhupBb9DqqO52DAlNPj4N4iV4VGYSdi1z9nABGPvEZBClsEfAFutvrL2dWFb5L3XO8klJTkVFfB73UG7DtUCznknB5qpQQhWxpTLv7F17FEwdj1qIracKlvpthyQGKCersDX92ZCXTJGWjIZCJX2HZBi5l80ZD';
+        const bearerToken = 'EAANuQX9U2lIBOZC7M3X64ZB87MGvjGkItT7lIEYsDzHGDGwD9FgZCj9ZAXbiQwJZARWpL9bh65ZCkDpcdOCG8YDKyfGVedaSKZBMdyVMtPG7o2mPXmBGRhrkaP3SAADqyWSQnmzTQAGcmvP5sAsmHrDW3olwYou6ZCxUDzK9mN2WQoThpnviqJAGH0VkZBTOBNBQbr8SQo8ey2SX5bqU9V8pd6AZDZD';
 
         // Define the headers object with the Authorization header
         const headers = {
@@ -109,13 +109,13 @@ async function SendWhatsApp(mobileNo) {
         };
 
         // Make a POST request to the third-party API with headers
-        const response = await axios.post('https://graph.facebook.com/v17.0/116356421335248/messages', postData, { headers });
+        const response = await axios.post("https://graph.facebook.com/v17.0/151336124726364/messages", postData, { headers });
 
         // Handle the response data here
         const data = response.data;
 
         console.log(data);
-        console.log("Message Sent To WhatApp Successfully");
+        console.log("Message Sent To WhatsApp Successfully");
     } catch (error) {
         console.log("error", error)
     }
