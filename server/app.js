2d6e41ee4d1962a9a61a091844f5b60dd396e1d5
Ritik kumar <ritikkumarkashyap9@gmail.com>
2023-10-24 22:00:08 +0530

    Rename Directory Commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require('dotenv').config()
+const app = express();
+const cors = require('cors')
+require("./db/config");
+require('./cron/cron');
+
+const ClientRouter = require('./routes/ClientRoute')
+
+
+//PORT
+const port = process.env.PORT;
+
+
+//body parser
+app.use(cors())
+app.use(express.json())
+app.use('/', ClientRouter.router);
+
+
+
+
+app.listen(port, () => {
+  console.log(`app listning on port ${port}`);
+
+});
+
