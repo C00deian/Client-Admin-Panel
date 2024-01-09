4f5808a628ecd0190301b48ced77e21e931ed8b4
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-01-10 02:19:08 +0530

    build Push

--- client/src/components/constent.js
+++ client/src/components/constent.js
@@ -1,2 +1,2 @@
-  //export const BASEURL = 'http://65.0.125.57:8080/';
-export const BASEURL = 'http://localhost:8080/';
+  export const BASEURL = 'http://65.0.125.57:8080/';
+// export const BASEURL = 'http://localhost:8080/';
